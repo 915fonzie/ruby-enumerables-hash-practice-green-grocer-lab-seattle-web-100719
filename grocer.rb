abeda7101af6d98712100feac0364f6b3203be9e
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-10-05 09:56:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,6 +8,9 @@ def consolidate_cart(cart)
   end 
 end 
 new_cart 
+end
+
+def apply_coupons(cart, coupons)
   coupons.each do |coupon| 
     coupon.each do |attribute, value| 
       name = coupon[:item] 
@@ -25,10 +28,6 @@ new_cart
   end 
 end 
   cart 
-end
-
-def apply_coupons(cart, coupons)
-  # code here
 end
 
 def apply_clearance(cart)
