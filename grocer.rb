da9add1a932c0adae71254cf0b044a4d8df0c467
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-10-05 10:46:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ def apply_coupons(cart, coupons)
    item = coupon[:item]
    if cart[item]
      if cart[item][:count] >= coupon[:num] && !cart.has_key["#{item} W/COUPON"]
-       cart["#{item} W/COUPON"] = [price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance],count: ]
+       cart["#{item} W/COUPON"] = [price: coupon[:cost] / coupon[:num], clearance: cart[item][:clear],count: coupon[:num]]
 
 end
 
