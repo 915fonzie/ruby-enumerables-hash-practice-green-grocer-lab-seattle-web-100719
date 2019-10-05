79c1e00568bc1991489071a5b25f44d99c2f32ff
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-10-05 11:37:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,4 +40,6 @@ applied_coupons = apply_coupons(hash_cart, coupons)
 applied_clearance = apply_clearance(applied_coupons)
 
 total = applied_clearance.reduce(0) {|acc, {key, value}| acc += value[:price] * value[:count]}
+
+total > 100 ? total * 0.9
 end
