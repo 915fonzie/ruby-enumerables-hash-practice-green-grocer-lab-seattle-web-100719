0f32c9ee73e56d417793db2e9e819641ca906f00
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-10-05 11:10:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,8 +19,10 @@ def apply_coupons(cart, coupons)
      else if cart[item][:count] >= coupon[:num] && cart.has_key["#{item} W/COUPON"]
        cart["#{item} W/COUPON"][:count] += coupon[:num]
      end
-     cart[item][:count] -= 
+     cart[item][:count] -= coupon[:num]
    end
+ end
+ cart
 end
 
 def apply_clearance(cart)
